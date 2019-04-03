4afba2fa2d1598ee0bcc69d5d00464f8c1d37466
Philippe <philippecappelle1@gmail.com>
2019-04-03 09:23:24 +0200

    Added cors

--- src/main/java/be/xplore/conference/rest/controller/RoomController.java
+++ src/main/java/be/xplore/conference/rest/controller/RoomController.java
@@ -1,11 +1,8 @@
 package be.xplore.conference.rest.controller;
 
-import be.xplore.conference.model.Room;
 import be.xplore.conference.rest.dto.RoomDto;
 import be.xplore.conference.service.RoomService;
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
