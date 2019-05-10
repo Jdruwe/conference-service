946535105ea90653fcad0476b49dc931ed7c657e
Pieter-JanKdg <pieterjan.noe@studenkt.kdg.be>
2019-05-10 10:19:10 +0200

    updated checkstyle file

--- src/test/java/be/xplore/conference/rest/controller/ClientControllerTest.java
+++ src/test/java/be/xplore/conference/rest/controller/ClientControllerTest.java
@@ -28,7 +28,6 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 import static org.hamcrest.Matchers.containsString;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
